59b4245bb48bf1fa5718606ec9b7bafb2ff4bb0e
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2025-03-27 23:18:30 +0100

    next

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,43 @@
 
+fun ffff(a: Int, b: Int): Boolean {
+
+    val max = if (a > b) {
+        println("a is larger than b.")
+        a
+    } else {
+        println("b is larger than a.")
+        return false;
+    }
+
+    val result: Int = max;
+    return result == 0;
+}
+
 fun ggg(count: Int): String {
+
     return "$count"
 }
 
 fun main(args: Array<String>) {
     println("Hello, Kotlin!");
+
+    for (i in 1..3) {
+        println("i => $i");
+    }
+
+    var language = arrayOf("Ruby", "Kotlin", "Python", "Java")
+
+    for (item in language) {
+
+        // printing array elements having even index only
+        if (itemx                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      %2 == 0)
+            println(language)
+    }
+
+//    print("Enter text: ")
+//    val stringInput = readlnOrNull();
+//    println("You entered: $stringInput")
+
 
     // args.len
     println("{args.size}");
@@ -18,4 +51,9 @@ fun main(args: Array<String>) {
     for (ttt in args) {
         println("argument => $ttt");
     }
+
+    val rrrrr: String? = "dsada";
+    val fgggg = args.getOrNull(2)
+
+
 }
